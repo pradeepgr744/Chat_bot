fe268fdf7442f8d47f382d250980d68474832c45
pradeepgr744 <dseeeap2020@gmail.com>
2023-12-21 18:06:39 +0530

    npm type animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./index.css";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
+import { TypeAnimation } from 'react-type-animation';
 
 const Chatbot = () => {
   const [loading, setLoading] = useState(false);
@@ -49,17 +49,28 @@ const Chatbot = () => {
     const text = ref.current.textContent;
     navigator.clipboard.writeText(text);
   };
+  const elemRef = useRef(null);
 
- 
+  useEffect(() => {
+  const elem = elemRef.current;
+  
+  setInterval(() => {
+  elem.scrollTop = elem.scrollHeight;
+  }, 5000);
+  }, []);
+
 
   return (
     <div className={`chatbot p-4 rounded-lg shadow-md h-[100dvh] flex flex-col text-lg sm-[100%]  md:w-[100%] xl:w-[80%] m-auto ${bgcolor ? "transition ease-in-out duration-300 bg-[#283142] " : "transition ease-in-out duration-300 bg-gray-200"}`}>
       <header className={`flex items-center justify-between border-b pb-4 ${bgcolor ? "text-white border-white" : "text-black border-black"}`}>
         <h1 className="text-xl font-bold">Chatbot</h1>
-        <label className="relative inline-flex items-center cursor-pointer">
-          <input type="checkbox" value="" className="sr-only peer" onClick={() => setbgcolor(!bgcolor)} />
-          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-400 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-black"></div>
-        </label>
+        <div className="flex gap-10 cursor-pointer">
+          <img src="/static/images/clear.svg" alt="" onClick={() => { setMessages([]) }}
+            className="w-5" />
+          <label className="relative inline-flex items-center ">
+            <input type="checkbox" value="" className="sr-only peer" onClick={() => setbgcolor(!bgcolor)} />
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-400 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-black"></div>
+          </label></div>
       </header>
 
 
@@ -73,7 +84,16 @@ const Chatbot = () => {
             <button onClick={copy}
               className="flex justify-end w-[100%]"
             ><img src="/static/images/copy_icon.svg" alt="" /></button>
-            <pre ref={ref} className="whitespace-pre-line">{message.parts}</pre>
+            <pre ref={ref} className="overflow-y-auto">
+              <TypeAnimation
+              cursor={false}
+              sequence=
+              {[message.parts]}
+
+              wrapper="span"
+              speed={{ type: 'keyStrokeDelayInMs', value: 30 }}
+              style={{ whiteSpace: 'pre-line' }}
+            /></pre>
           </div>
         ))}
       </div>
@@ -88,20 +108,20 @@ const Chatbot = () => {
           required
         />
 
-          {loading ?
-            <div role="status" className=" bg-blue-500 text-white w-[12%] flex justify-center h-auto items-center rounded-lg">
-              <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-              </svg>
-              <span class="sr-only">Loading...</span>
-            </div>:
+        {loading ?
+          <div role="status" className=" bg-blue-500 text-white w-[12%] flex justify-center h-auto items-center rounded-lg">
+            <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+            </svg>
+            <span class="sr-only">Loading...</span>
+          </div> :
           <button type="submit" className=" bg-blue-500 text-white w-[12%] flex justify-center h-auto items-center rounded-lg">
-          <img src="/static/images/send.svg" alt="" />
-        </button>  
-        
-        
-          }
+            <img src="/static/images/send.svg" alt="" />
+          </button>
+
+
+        }
       </form>
     </div>
   );
